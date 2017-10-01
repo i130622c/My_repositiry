e69112fa111ebef89e795bfcb1fd94f88f40e1fb
miyashiro_t <i130622c@gmail.com>
2017-10-01 19:17:43 +0900

    [003] Presentation Error

--- AIZU_0/src/is_it_a_Right_Triangle/Main.java
+++ AIZU_0/src/is_it_a_Right_Triangle/Main.java
@@ -1,34 +1,27 @@
 package is_it_a_Right_Triangle;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-	public static void main(String args[]){
-		Scanner scanner = new Scanner(System.in);
-		int dataSet = scanner.nextInt();
-		List<String> result = new ArrayList<>();
-		for(int i=0; i<dataSet; i++){
-			int ab = (int)(Math.pow(scanner.nextInt(),2));
-			int bc = (int)(Math.pow(scanner.nextInt(),2));
-			int ac = (int)(Math.pow(scanner.nextInt(),2));
-			String yes = "YES";
-			String no = "NO";
-			if(ab==bc+ac){
-				result.add(yes);
-			}else if(bc==ab+ac){
-				result.add(yes);
-			}else if(ac==ab+bc){
-				result.add(yes);
+	public static void main(String args[]) throws IOException{
+		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		int n = Integer.parseInt(bufferedReader.readLine());
+		StringBuilder answer = new StringBuilder();
+		for(int i=0; i<n; i++){
+			String[] dataSet = bufferedReader.readLine().split(" ");
+			double x = Math.pow(Integer.parseInt(dataSet[0]), 2);
+			double y = Math.pow(Integer.parseInt(dataSet[1]), 2);
+			double z = Math.pow(Integer.parseInt(dataSet[2]), 2);
+			
+			if(x+y==z || x+z==y || y+z==x){
+				answer.append("YES\n");	
+			}else{
+				answer.append("NO\n");	
 			}
-			result.add(no);
 		}
-		
-		for(int j=0; j<dataSet;j++){
-			System.out.println(result.get(j));
-		}
-		scanner.close();
-		
+		System.out.println(answer);
 	}	
 }
